aa6df33f54c602424b0f6507605c9ae5160ec80e
hq <hq_cml@163.com>
2020-01-21 22:53:12 +0800

    快速排序

--- sort/sort.c
+++ sort/sort.c
@@ -14,6 +14,33 @@ void bubbleSort(int a[], int len) {
     }
 }
 
+//快速排序
+void quickSort(int a[], int low, int high) {
+    if (low >= high) {
+        return;
+    }
+    int mid = partition(a, low, high);
+    quickSort(a, mid+1, high);   //recursion
+    quickSort(a, low, mid-1);
+}
+//5 6 2 1 8 10 9  - 5
+int partition(int a[], int low, int high) {
+    int val = a[low];
+    int tmp;
+    while(low < high) {
+        while (low < high && val <= a[high]) {
+            high --;
+        }
+        a[low] = a[high];
+        while (low < high && a[low] <= val) {
+            low ++;
+        }
+        a[high] = a[low];
+    }
+    a[low] = val;
+    return low;
+}
+
 void foreachArr(int a[], int len) {
     int i;
     for(i=0; i<len; i++) {
@@ -23,7 +50,10 @@ void foreachArr(int a[], int len) {
 }
 
 int main() {
-    int a[] = {2, 3, 1, 4, 1, 6, 9, 7};
-    bubbleSort(a, 8);
-    foreachArr(a, 8);
+    //int a[] = {2, 3, 1, 4, 1, 6, 9, 7};
+    //bubbleSort(a, 8);
+
+    int a[] = {5, 6, 2, 1, 8, 10, 9};
+    quickSort(a, 0, 6);
+    foreachArr(a, 7);
 }
