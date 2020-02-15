f84111749b32b8f0569c7d39ad5c83b9bee68a58
hq <hq_cml@163.com>
2020-02-15 16:01:47 +0800

    二维数组查找

--- array/arr.c
+++ array/arr.c
@@ -54,6 +54,36 @@ int find2DimArr(int **a, int rows, int cols, int n) {
     return 0; //没找到
 }
 
+//用一维数组代替二维数组
+//a[x][y] == a[x + y*cols]
+int findDoubleArr(int *a, int rows, int cols, int n) {
+    int x = cols -1;
+    int y = 0;
+    while(x >= 0 && y < rows) {
+        while(a[x + y*cols] > n && x >=0)
+            x--;
+        if (x < 0) {
+            printf("Not Find\n");
+            return 0;
+        }
+        if (a[x + y*cols] == n) {
+            printf("Find %d in [%d, %d]\n", n, y, x);
+            return 1;
+        }
+        while(a[x + y*cols] < n && y < rows)
+            y++;
+        if (y >= rows) {
+             printf("Not Find\n");
+             return 0;
+        }
+        if (a[x + y*cols] == n) {
+             printf("Find %d in [%d, %d]\n", n, y, x);
+             return 1;
+        }
+    }
+    printf("Not Find\n");
+    return 0;
+}
 
 int main() {
 //    int a[] = {1, 2, 3, 4, 2, 3, 7};
@@ -62,11 +92,15 @@ int main() {
     int a[4][4] = {
         {1, 2, 8, 9},
         {2, 4, 9, 12},
-        {5, 7, 10, 13},
-        {6, 8, 11, 12},
+        {7, 8, 10, 13},
+        {8, 9, 11, 12},
     };
-    find2DimArr((int**)a, 4, 4, 1);
-    find2DimArr((int**)a, 4, 4, 6);
-    find2DimArr((int**)a, 4, 4, 7);
-    find2DimArr((int**)a, 4, 4, 16);
+//    find2DimArr((int**)a, 4, 4, 1);
+//    find2DimArr((int**)a, 4, 4, 6);
+//    find2DimArr((int**)a, 4, 4, 7);
+//    find2DimArr((int**)a, 4, 4, 16);
+    findDoubleArr((int*)a, 4, 4, 1);
+    findDoubleArr((int*)a, 4, 4, 6);
+    findDoubleArr((int*)a, 4, 4, 7);
+    findDoubleArr((int*)a, 4, 4, 15);
 }
