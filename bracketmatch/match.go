23be184bccff9d99ac2371767ed526d8e7e2259f
hq <hq_cml@163.com>
2020-02-22 16:11:35 +0800

    括号匹配改进

--- bracketmatch/match.go
+++ bracketmatch/match.go
@@ -9,40 +9,36 @@ func CheckSign(str string) bool {
     var mystack stack.Stack
     for _, t := range str {
         v := string(t)
-        val, ok := mystack.Top()
-        if !ok {
-            mystack.Push(v)
-            continue
-        }
-        topVal, _ := val.(string)
-
-        switch string(v) {
+        switch (v) {
         case ")":
-            if topVal == "(" {
-                mystack.Pop()
-            } else if topVal == "[" || topVal == "{"{
-                fmt.Println("Fuck 1")
-                return false
-            } else {
+            val, ok := mystack.Pop()
+            if !ok {
                 mystack.Push(v)
+                continue
+            }
+            topVal, _ := val.(string)
+            if topVal != "(" {
+                return false
             }
         case "]":
-            if topVal == "[" {
-                mystack.Pop()
-            } else if topVal == "(" || topVal == "{"{
-                fmt.Println("Fuck 2")
-                return false
-            } else {
+            val, ok := mystack.Pop()
+            if !ok {
                 mystack.Push(v)
+                continue
+            }
+            topVal, _ := val.(string)
+            if topVal != "[" {
+                return false
             }
         case "}":
-            if topVal == "{" {
-                mystack.Pop()
-            } else if topVal == "[" || topVal == "("{
-                fmt.Println("Fuck 3")
-                return false
-            } else {
+            val, ok := mystack.Pop()
+            if !ok {
                 mystack.Push(v)
+                continue
+            }
+            topVal, _ := val.(string)
+            if topVal != "{" {
+                return false
             }
         default:
             mystack.Push(v)
@@ -50,8 +46,15 @@ func CheckSign(str string) bool {
     }
 
     if mystack.Len() > 0 {
-        return false
+       return false
     } else {
-        return true
+       return true
     }
+}
+
+func main() {
+    fmt.Println(CheckSign("((("))
+    fmt.Println(CheckSign("((()))"))
+    fmt.Println(CheckSign("([()]}"))
+    fmt.Println(CheckSign("{[()]}"))
 }
